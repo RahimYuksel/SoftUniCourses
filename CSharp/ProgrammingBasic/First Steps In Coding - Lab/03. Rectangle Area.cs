1f3bd6a3ce12ce15ee4fc3b8e696087af8a16c25
Rahim Yuksel Halil <rhmyksl@gmail.com>
2023-02-20 13:35:37 +0200

    Create 03. Rectangle Area.cs

---
+++ CSharp/ProgrammingBasic/First Steps In Coding - Lab/03. Rectangle Area.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Hello_Softuni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(a * b);
+
+        }
+    }
+}
