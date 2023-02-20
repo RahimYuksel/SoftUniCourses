1100a76ef96adc227fc680d3d9ac2ea4af2c0a37
Rahim Yuksel Halil <rhmyksl@gmail.com>
2023-02-20 13:40:06 +0200

    Create 09. Yard Greening.cs

---
+++ CSharp/ProgrammingBasic/First Steps In Coding - Lab/09. Yard Greening.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Hello_Softuni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double kvadrat = double.Parse(Console.ReadLine());
+            double final = kvadrat * 7.61;
+
+            double otstapka = 0.18 * final;
+            double fincen = final - otstapka;
+
+
+
+
+            Console.WriteLine($"The final price is: {fincen} lv.");
+                Console.WriteLine($"The discount is: {otstapka} lv.");
+
+
+        }
+    }
+}
