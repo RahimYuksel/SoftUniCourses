4bc6f6334048844ecda2c8805106e3da4ff213c0
Rahim Yuksel Halil <rhmyksl@gmail.com>
2023-02-20 13:39:07 +0200

    Create 07. Projects Creation.cs

---
+++ CSharp/ProgrammingBasic/First Steps In Coding - Lab/07. Projects Creation.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Hello_Softuni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = (Console.ReadLine());
+            int time = int.Parse(Console.ReadLine());
+           
+
+            Console.WriteLine($"The architect {firstName} will need {time * 3} hours to complete {time} project/s."
+);
+
+
+        }
+    }
+}
