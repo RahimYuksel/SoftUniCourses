852c0ee9c6706b85abb59b080c380b231396f27f
Rahim Yuksel Halil <rhmyksl@gmail.com>
2023-02-20 13:36:50 +0200

    Create 05. Greeting by Name.cs

---
+++ CSharp/ProgrammingBasic/First Steps In Coding - Lab/05. Greeting by Name.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Hello_Softuni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = (Console.ReadLine());
+
+            Console.WriteLine($"Hello, {name}!");
+            
+
+
+
+        }
+    }
+}
